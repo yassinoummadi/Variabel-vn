4b81cf486eae8d095c665c6215374c752a495c39
Yassin Oummadi <yassinoi@vgy.se>
2016-08-30 14:44:14 +0200

    Övning1-5

--- Var1-6/Program.cs
+++ Var1-6/Program.cs
@@ -10,6 +10,67 @@ namespace Var1_6
     {
         static void Main(string[] args)
         {
+            /*
+   Övning 1
+Console.WriteLine("Mata in tal 1: ");
+string inmatat;
+inmatat = Console.ReadLine();
+int Heltal = int.Parse(inmatat);
+Console.WriteLine("Mata in tal 2: ");
+
+inmatat = Console.ReadLine();
+double Decimaltal = double.Parse(inmatat);
+Console.WriteLine("Summan: " + (Heltal + Decimaltal));
+Console.WriteLine("Produkten: " + (Heltal * Decimaltal));
+Console.WriteLine("Kvoten: " + (Heltal / Decimaltal));
+
+Console.ReadLine();
+*/
+            /* Övning 2
+            Console.WriteLine("Mata in ditt förnamn:");
+            string inmatat;
+            inmatat = Console.ReadLine();
+            Console.WriteLine("Mata in ditt efternamn:");
+            string efternamn;
+            efternamn = Console.ReadLine();
+            Console.WriteLine("Hejsan Mr. " + inmatat + " " + efternamn);
+            Console.ReadLine();
+
+            */
+
+            /* Övning 3
+            
+            Console.WriteLine("Mata in grader i Celsius: ");
+            string celsius = Console.ReadLine();
+            int grader = int.Parse(celsius);
+            Console.WriteLine("Det blir" + " " + (1.8 * grader + 32) + " " + "grader Farenheit");
+            Console.ReadLine();
+
+            */
+            /* Övning 4
+            Console.Write("Mata in grader i Celsius: ");
+            string celsius = Console.ReadLine();
+            int grader = int.Parse(celsius);
+            Console.Write("Det blir" + " " + (1.8 * grader + 32) + " " + "grader Farenheit.");
+            Console.ReadLine();
+            */
+            /* Övning 5
+            Console.Write("Cirkelns radie:");
+            double radie = Console.Read();
+            Console.WriteLine("Area:" + (radie * radie * 3.14));
+            Console.WriteLine("Omkrets:" + (radie + radie * 3.14));
+            Console.ReadLine();
+            */
+            /* Övning 6 (INTE KLAR)
+            Console.Write("Mata in ett tresiffrigt tal:");
+            int tal1 = Console.ReadLine();
+            int tal2 = Console.ReadLine();
+            int tal3 = Console.ReadLine();
+            Console.WriteLine("Siffersumman är: " + (tal1 + tal2 + tal3));
+            Console.ReadLine();
+            */
+
+            //Övning6
         }
     }
 }
